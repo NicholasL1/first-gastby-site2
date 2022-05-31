7e41e0e58856876e04b9b228f19f577fe10fac1f
Nicholas Lachhman <NLachhman@goeasy.com>
2022-05-31 17:58:27 -0400

    Finished p4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,29 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle,
+} from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
-  return (
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -18,6 +36,11 @@ const Layout = ({ pageTitle, children }) => {
               About
             </Link>
           </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
@@ -27,5 +50,4 @@ const Layout = ({ pageTitle, children }) => {
     </div>
   )
 }
-
 export default Layout
